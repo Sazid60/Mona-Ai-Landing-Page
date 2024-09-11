430009c75276af11920d133b646f6fee451bee12
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-09-11 19:51:51 +0600

    Some Bug Fix

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,8 +1,8 @@
 
 const ErrorPage = () => {
     return (
-        <div>
-            <h1>This Is Error Page</h1>
+        <div className='flex justify-center items-center min-h-screen flex-col text-center bg-[#131B2D]'>
+            <h1><span className='font-bold text-4xl'>What’s up? </span><br />Design For <span className='text-red-600'>{page}</span> Page Was Not Given.</h1>
         </div>
     );
 };
